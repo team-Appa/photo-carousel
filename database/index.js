4a14b5b29111cff410dc711061ac485712110edd
Vasan Arul <vasankarul@gmail.com>
2020-07-11 11:33:32 -0700

    Committed code should reflect working Sequelize-Postgres connection

--- database/index.js
+++ database/index.js
@@ -1,5 +1,21 @@
-const mongoose = require('mongoose');
-const dbconnection = mongoose.connect('mongodb://localhost/products');
+// const mongoose = require('mongoose');
+// const dbconnection = mongoose.connect('mongodb://localhost/products');
 
-const db = mongoose.connect('dbconnection');
-module.exports = db;
+// const db = mongoose.connect('dbconnection');
+// module.exports = db;
+
+const {Sequelize} = require('sequelize');
+
+const sequelize = new Sequelize('postgres', 'postgres', 'baseball', {
+  host: 'localhost',
+  dialect: 'postgres'
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
