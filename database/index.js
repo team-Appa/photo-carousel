ac7ca61b82dd21c83fbab694dc4c31eee89732ee
Vasan Arul <vasankarul@gmail.com>
2020-07-22 11:02:44 -0700

    Committed code should reflect removal of Mongoose connections

--- database/index.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-const dbconnection = mongoose.connect('mongodb://localhost/products');
-
-const mong = mongoose.connect('dbconnection');
-module.exports = mong;
